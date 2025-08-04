d9579c9d41760fe8388d58f56cbba1442e08659a
Charlotte Roe <charlotte.kj.roe@gmail.com>
2025-08-04 14:34:09 +0100

    Add text styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,9 @@
 <template>
-    <h1>home</h1>
+    <h1>heading level 1</h1>
+    <h2>heading level 2</h2>
+    <h3>heading level 3</h3>
+    <h4>heading level 4</h4>
+    <p class="subheading">text subheading</p>
+    <p>body text</p>
+    <a href="#">link</a>
 </template>
