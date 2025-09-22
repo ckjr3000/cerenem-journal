5eee19184e0ba71bd43c437da3a6ca5a4a55464f
Charlotte Roe <66831847+ckjr3000@users.noreply.github.com>
2025-09-22 13:19:11 +0100

    Add page accent to home page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts"></script>
 
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
-<style scoped></style>
+<style scoped>
+body::before {
+  content: "";
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 400px; /* adjust size */
+  height: 400px; /* adjust size */
+  background: url("../assets/vectors/page-accent.svg") no-repeat top right;
+  background-size: contain;
+
+  pointer-events: none; /* so it doesn’t block clicks */
+  z-index: 0; /* baseline layer */
+}
+</style>
