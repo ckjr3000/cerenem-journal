dadc8e9efd8e6c09b77236da150384b264d5463a
Charlotte Roe <66831847+ckjr3000@users.noreply.github.com>
2025-09-22 13:58:12 +0100

    Organise home page layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,52 +6,56 @@
       alt=""
       class="page-decoration"
     />
-    <h1>CeReNeM Journal #9</h1>
-    <h2>Introduction</h2>
-    <p class="subheading">x minute read</p>
-    <AudioPlayer src="/Soundbetterbass.wav" />
-    <p>
-      Research is often a solitary endeavor, requiring focused study and
-      individual reflection. Yet the communities that form within universities
-      play a vital role in shaping this process, offering support, dialogue, and
-      shared experience that enrich the researcher’s journey. Our time at the
-      Centre for Research in New Music (CeReNeM) at the University of
-      Huddersfield has profoundly shaped our perspectives and music-making,
-      inspiring us to co-edit this edition of the CeReNeM Journal to document
-      and energize this vibrant community.
-    </p>
-    <p>
-      For Issue Nine, our vision was to approach research as a communal act.
-      Given CeReNeM’s wide-ranging interests and specialisms (from composers and
-      improvisers, to sound artists, to creative coders and software
-      developers), the challenge lay in uniting these varied voices without
-      asking anyone to stray from their unique research paths. To do this, we
-      adopted the metaphor of 'islands' as our guiding framework. In the course
-      of our early conversations, three islands emerged: score composition,
-      sound installation, and improvisation.
-    </p>
-    <p>
-      As diverse as these areas can be, through our collaborations they
-      converged into a shared exploration—an invitation to consider and
-      reconsider the key questions that motivated our intentions: How do
-      individual insights contribute to a collective narrative? What does it
-      mean to navigate the boundaries between individual experimentation and
-      communal engagement? 
-    </p>
-    <p>
-      As you consider these questions, we invite you to explore the projects
-      that reflect and celebrate our community of researchers at CeReNeM in
-      2024.
-    </p>
-    <nav>
-      <router-link to="/scorecomposition"
-        >Score Composition Project</router-link
-      >
-      <router-link to="/soundinstallation"
-        >Sound Installation Project</router-link
-      >
-      <router-link to="/improvisation">Improvisation Project</router-link>
-    </nav>
+    <div id="left">
+      <h1>CeReNeM Journal #9</h1>
+      <h2>Introduction</h2>
+      <p class="subheading">x minute read</p>
+      <AudioPlayer src="/Soundbetterbass.wav" />
+      <p>
+        Research is often a solitary endeavor, requiring focused study and
+        individual reflection. Yet the communities that form within universities
+        play a vital role in shaping this process, offering support, dialogue,
+        and shared experience that enrich the researcher’s journey. Our time at
+        the Centre for Research in New Music (CeReNeM) at the University of
+        Huddersfield has profoundly shaped our perspectives and music-making,
+        inspiring us to co-edit this edition of the CeReNeM Journal to document
+        and energize this vibrant community.
+      </p>
+      <p>
+        For Issue Nine, our vision was to approach research as a communal act.
+        Given CeReNeM’s wide-ranging interests and specialisms (from composers
+        and improvisers, to sound artists, to creative coders and software
+        developers), the challenge lay in uniting these varied voices without
+        asking anyone to stray from their unique research paths. To do this, we
+        adopted the metaphor of 'islands' as our guiding framework. In the
+        course of our early conversations, three islands emerged: score
+        composition, sound installation, and improvisation.
+      </p>
+      <p>
+        As diverse as these areas can be, through our collaborations they
+        converged into a shared exploration—an invitation to consider and
+        reconsider the key questions that motivated our intentions: How do
+        individual insights contribute to a collective narrative? What does it
+        mean to navigate the boundaries between individual experimentation and
+        communal engagement? 
+      </p>
+      <p>
+        As you consider these questions, we invite you to explore the projects
+        that reflect and celebrate our community of researchers at CeReNeM in
+        2024.
+      </p>
+    </div>
+    <div id="right">
+      <nav>
+        <router-link to="/scorecomposition"
+          >Score Composition Project</router-link
+        >
+        <router-link to="/soundinstallation"
+          >Sound Installation Project</router-link
+        >
+        <router-link to="/improvisation">Improvisation Project</router-link>
+      </nav>
+    </div>
   </main>
 </template>
 
@@ -66,3 +70,23 @@ export default defineComponent({
   components: { AudioPlayer, Header },
 });
 </script>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+#left {
+  width: 45vw;
+}
+
+nav {
+  margin-top: 250px;
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+}
+</style>
