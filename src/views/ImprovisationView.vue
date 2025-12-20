282af75001cb4aceb9143ea4ec764b5ec7d6f801
Charlotte Roe <66831847+ckjr3000@users.noreply.github.com>
2025-12-20 11:27:10 +0000

    Improv page text

--- src/views/ImprovisationView.vue
+++ src/views/ImprovisationView.vue
@@ -1,6 +1,303 @@
 <template>
   <Header />
-  <h1>improv</h1>
+  <img src="../assets/vectors/page-accent.svg" alt="" class="page-decoration" />
+  <main>
+    <h1>The Group Improvisation</h1>
+    <h2>Page Contents</h2>
+    <nav class="page-nav">
+      <a href="#overview">Overview</a>
+      <a href="#">Performance</a>
+      <a href="#reflections">Reflections</a>
+    </nav>
+    <div class="project-section" id="overview">
+      <h2>Overview</h2>
+      <p class="subheading">Listen to text</p>
+      <img src="../assets/vectors/landmark-down.svg" alt="" class="landmark" />
+      <AudioPlayer src="text-audio/introduction.mp3" />
+      <p class="subheading">Or read (1 minute)</p>
+      <img src="../assets/vectors/landmark-down.svg" alt="" class="landmark" />
+      <p>
+        Jean-Luc Guionnet and Seijiro Murayama came to the University of
+        Huddersfield to lead an improvisation workshop. The workshop allowed us
+        to explore new methods of sound-making, and taught us about sharing
+        space with different performers. From this, we took part in a group
+        performance at the CeReNeM Journal Showcase.
+      </p>
+      <div class="spacer">
+        <img src="../assets/vectors/cross-roads-spacer.svg" alt="" />
+        <a href="#">Back to contents</a>
+      </div>
+    </div>
+    <div class="project-section" id="performance">
+      <h2>Performance</h2>
+      <!-- content -->
+    </div>
+    <div class="project-section" id="reflections">
+      <h2>Reflections</h2>
+      <p class="subheading">Listen to text</p>
+      <img src="../assets/vectors/landmark-down.svg" alt="" class="landmark" />
+      <AudioPlayer src="text-audio/introduction.mp3" />
+      <p class="subheading">Or read (1 minute)</p>
+      <img src="../assets/vectors/landmark-down.svg" alt="" class="landmark" />
+      <h3>Freya</h3>
+      <p>
+        I played a flamenco guitar for my solo improvisation [...] for the most
+        part, I avoided traditional flamenco techniques, instead using a cello
+        bow for scraping and creaking sounds
+      </p>
+      <p>
+        [My] approach was inspired by the workshop by Jean-Luc Guionnet and
+        Seijiro Murayama, who discussed techniques for detaching from an
+        instrument’s conventional idioms and breaking free from traditional
+        constraints. This does not preclude common idioms but encourages
+        reconceptualisation of how an instrument is approached.
+      </p>
+      <p>
+        Despite my inexperience with contemporary on-stage improvisation
+        compared to my peers, I was pleased with the performance’s haunting
+        soundscape.
+      </p>
+      <h3>Jo</h3>
+      <p>Start with white and add colour</p>
+      <p>The space makes the sound</p>
+      <br />
+      <p>The solo is always playing inside you</p>
+      <br />
+      <p>Listen to other people.</p>
+      <br />
+      <p>Coming into silence is very imposing</p>
+      <br />
+      <p>
+        Improvising for me has been about creating a dense atmosphere, the
+        specifics of the sound aren't always the most important element…
+      </p>
+      <h3>Sophie</h3>
+      <p>An interesting experience!</p>
+      <p>
+        Seijiro Murayama had us taking part in movement exercises that I
+        personally found a bit cringe worthy and something I would normally run
+        miles away from taking part in due to deep rooted, long term
+        embarrassment associated with the body and early childhood experiences.
+        Not for me, basically! Physically navigating around the other people in
+        the group and the room itself can be seen as similar to how sound and
+        music moves around an auditory space during an improvisation. You’ve got
+        to be careful not to trip someone else up.
+      </p>
+      <p>
+        Personally, the most useful feedback I received from Jean-Luc was a
+        comment about how I tend to create a groove / loop when improvising on
+        the trombone which becomes a sort of sonic-trap that is hard to escape
+        from, and I agree with this.
+      </p>
+      <p>
+        I suppose the answer is, knowing that I have this tendency, to practice
+        improvisation tricks a bit more and either look for obvious exit signs
+        or just go wild and smash through the walls
+      </p>
+      <h3>Tom</h3>
+      <p>
+        One of the biggest challenges I face as an improviser and composer is
+        working out what my music is.
+      </p>
+      <p>
+        Prior to the workshop with Seijiro and Jean-Luc, I would have described
+        improvising as a difficult balance between making my music and allowing
+        everyone else’s music space to breathe.
+      </p>
+      <p>
+        The main thing I learnt from this experience was that I can make my own
+        music and at the same time provide space for others to make their music.
+      </p>
+      <p>
+        My music or ‘necessity’ as Seijro put it feels somewhat irrelevant and
+        what now feels significant is when my music occurs
+      </p>
+      <p>
+        I am still full of questions, but these are focused around a positive
+        angle: what can my music become with the company of others? This is
+        something which fills me with optimism about the future of my music.
+      </p>
+      <p>
+        There is no doubt that a synthesiser can make a louder sound than an
+        acoustic cello, but if the aim of the improvisation is not a rise to a
+        climax, then the volume levels of these instruments may not be an issue.
+        Working together can unlock a new music which doesn’t compromise the
+        musical necessity of either instrument.
+      </p>
+      <h3>Anthony</h3>
+      <p>Improvisation</p>
+      <br />
+      <p>[My Necessity]</p>
+      <p>“Of course I have this background, but how to share?”</p>
+      <p>**Preparation, Desperation, Burden**</p>
+      <br />
+      <p>[Inside // Outside]</p>
+      <p>“Come from somewhere…not here…change the space”</p>
+      <p>**Interjection**</p>
+      <ul>
+        <li>Assasin</li>
+        <li>Digging</li>
+        <li>Event</li>
+        <li style="list-style: none">**Hiding**</li>
+        <li>Pulse</li>
+        <li>Imagination</li>
+      </ul>
+      <br />
+      <p>[Discontinuity // Continuity]</p>
+      <p>“Go your own way…connect when possible."</p>
+      <p>“Cutting…it gives a new vision.”</p>
+      <p>“And once again, start.”</p>
+      <br />
+      <p>[...]</p>
+      <br />
+      <p>“Music happens sometimes…”</p>
+      <h3>Charlotte</h3>
+      <p>PART 1: Not a musician</p>
+      <p>
+        It's strange to write about this now, because it's a sentiment I've left
+        behind, and was probably on the cusp of leaving behind at the time of
+        starting the improv workshop, but funnily enough I did begin my MA in
+        Music thinking of myself as 'not a musician'.
+      </p>
+      <p>Reasons for this classification:</p>
+      <ul>
+        <li>I dropped Music in Year 9</li>
+        <li>I've never had instrument lessons of any sort</li>
+        <li>I can't read or write traditional notation</li>
+        <li>I can't identify by ear: notes, chords, keys, time signatures</li>
+      </ul>
+      <p>
+        I think my path has actually been a relatively common one. I came from
+        visual art, into 'sound art', into noise-based music, and now that's
+        where my practice sits: noise, computer music, electronics, glitch etc.
+      </p>
+      <p>
+        By this time I had started performing some of my 'sound art', and was
+        coming round to the idea that I might be a musician, but definitely not
+        an improviser. All my performances were pre-planned and rehearsed.
+      </p>
+      <p>
+        The reason I mention this is I had gone into the workshop thinking, at
+        least for the first day, I would just listen and not take part. I wasn't
+        an improviser, so who knows what would be expected of me?
+      </p>
+      <p>That I take part, for one.</p>
+      <p>PART 2: A series of ERRORS</p>
+      <p>
+        So I took out my beloved decrepit old laptop, running a fresh install of
+        Ubuntu that I had yet to re-setup with SuperCollider & TidalCycles, and
+        tried to find something I could play.
+      </p>
+      <p>
+        I had a little digital instrument / browser tool / performance UI (I'm
+        still not sure what I call them) that I had recently built to perform
+        with called 'Clock'.
+      </p>
+      <p>
+        It's a grid of 365 buttons. A number which is related to the ✨ concept
+        ✨ of the piece for which it was built, but arbitrary for this new
+        purpose. Each button can be switched on to output white noise through a
+        band pass filter tuned to a frequency taken from a dataset containing
+        some numbers also only 🌼 meaningful 🌼 in another context.
+      </p>
+      <p>
+        There's some automation on the original version of Clock, blocks of
+        buttons are briefly switched on at set intervals throughout the
+        performance.
+      </p>
+      <p>
+        There are keyboard shortcuts to play: all the low, medium, or high
+        frequencies, a random frequency, a reverse of the currently selected
+        buttons, all the buttons, and to switch off all the currently playing
+        buttons.
+      </p>
+      <p>I decided I could use this as long as I removed the automation.</p>
+      <p>
+        The first improv began, everyone in a big circle doing solo-duo, I took
+        a spot in the circle where I could plug in my laptop and started to edit
+        the code. I think the first time I was meant to play I said I wasn't
+        ready, but the second I produced a very quiet little bunch of sounds
+        directly out of my laptop speakers.
+      </p>
+      <p>Woah - improvisation!</p>
+      <p>
+        I found time to improve my instrument's suitability for improv
+        throughout the day: standardising the frequency range to evenly spaced
+        values, and upping the max gain to I could get more volume out of my
+        laptop speakers.
+      </p>
+      <p>I think the latter is what broke the speaker.</p>
+      <p>
+        Half way through an exercise it started doing this, and honestly it was
+        super fun, I couldn't modulate the sound via Clock anymore so I just had
+        to physically move the computer around to have the sound bounce and
+        resonate in different ways.
+      </p>
+      <p>Improvisation 😎</p>
+      <p>
+        All good I thought, I have a shiny new MacBook and nice monitor at home,
+        I can bring this tomorrow, it will be my rig. I'll buy one of those hard
+        shell suitcases to transport it.
+      </p>
+      <p>
+        I locked my new suitcase with a code - and must have set it up wrong,
+        because I couldn't get back in. Luckily, Jo Christman always carries a
+        screwdriver, but I had to wait until lunch to break in and free my tech.
+      </p>
+      <p>PART 3: Metronome+</p>
+      <p>
+        Seijiro told me his favourite instrument to improvise with is a
+        metronome. I have a metronome app on my phone.
+      </p>
+      <p>Improvisation 💃</p>
+      <p>
+        What's great about using the metronome is you get to practice
+        constraint. I begin by thinking 'ok I can modulate the bpm and the
+        'voice', that's probably enough', but Seijiro encouraged me to stick
+        with one voice, and one (fairly slow) BPM.
+      </p>
+      <p>
+        I remember one solitary 'click' in the middle of some silence being one
+        of my favourite gestures I made during the workshop.
+      </p>
+      <p>
+        Structuring this writing how I have isn't to imply that I undertook some
+        kind of linear hero's journey from non-improviser to improviser over the
+        course of three days. I actually found the workshop immediately
+        engaging, accessible, and understandable.
+      </p>
+      <p>
+        I wonder if my lack of musician-y background helped me get into the idea
+        of non-idiomatic improv easier? I wonder if playing electronics over a
+        traditional instrument did?
+      </p>
+      <p>
+        There are a number of techniques and ideas I took away, and still think
+        about when improvising now.
+      </p>
+      <ul>
+        <li>
+          Having a constant internal 'tiktikitikitik' that you start/stop,
+          surface/bury.
+        </li>
+        <li>
+          Considering the end of a sound just as much of an event as the start
+          of one.
+        </li>
+        <li>
+          The idea of 'soupiness', the instinct to politely make space for every
+          other sound in the room, waiting your turn to duck in and out. This is
+          one approach, it isn't bad, but how do you prevent it from being the
+          default?
+        </li>
+      </ul>
+      <p></p>
+      <div class="spacer">
+        <img src="../assets/vectors/cross-roads-spacer.svg" alt="" />
+        <a href="#">Back to contents</a>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
@@ -13,3 +310,9 @@ export default defineComponent({
   components: { Header },
 });
 </script>
+
+<style>
+#reflections > p {
+  font-style: italic;
+}
+</style>
